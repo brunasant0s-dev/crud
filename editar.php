cb4e47286d4d4d9b6be9b64c1347d174e8ca870e
brunasant0s-dev <brunaceciliavitorio@gmail.com>
2023-09-18 14:42:24 -0300

    excluir e editar

--- editar.php
+++ editar.php
@@ -1,42 +1,69 @@
 <?php
-include 'conexao.php'; // Inclua o arquivo de conexão ao banco de dados
+//Vitória 
+include 'conexao.php';
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
-    // Recupere os dados do paciente com base no ID
-    $sql = "SELECT * FROM paciente WHERE id = $id";
-    $result = $mysqli->query($sql);
+    // Verifique se o formulário foi enviado
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Obtenha os dados do formulário
+        $nome = $_POST['nome'];
+        $cpf = $_POST['cpf'];
+        $convenio = $_POST['convenio'];
 
-    if ($result) {
-        if ($result->num_rows > 0) {
-            $paciente = $result->fetch_assoc();
+        // Atualize os dados do paciente no banco de dados
+        $sql = "UPDATE paciente SET nome_paciente = '$nome', cpf_paciente = '$cpf', convenio_paciente = '$convenio' WHERE id_paciente = $id";
+        if ($mysqli->query($sql)) {
+            echo "Dados do paciente atualizados com sucesso!";
         } else {
-            echo "Paciente não encontrado.";
-            exit;
+            echo "Erro ao atualizar os dados do paciente: " . $mysqli->error;
         }
+    }
+
+    // Recupere os dados do paciente para exibir no formulário de edição
+    $sql = "SELECT * FROM paciente WHERE id_paciente = $id";
+    $result = $mysqli->query($sql);
+    if ($result->num_rows > 0) {
+        $paciente = $result->fetch_assoc();
     } else {
-        echo "Erro ao buscar paciente: " . $mysqli->error;
-        exit;
+        echo "Paciente não encontrado";
     }
 } else {
     echo "ID do paciente não especificado.";
-    exit;
-}
-
-// Processo de edição do paciente aqui
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $nomeNovo = $_POST['nome'];
-    $cpfNovo = $_POST['cpf'];
-    $convenioNovo = $_POST['convenio'];
-
-    // Execute a consulta SQL para atualizar os dados do paciente
-    $sql = "UPDATE paciente SET nome_paciente = '$nomeNovo', cpf_paciente = '$cpfNovo', convenio_paciente = '$convenioNovo' WHERE id = $id";
-
-    if ($mysqli->query($sql)) {
-        echo "Paciente atualizado com sucesso.";
-    } else {
-        echo "Erro ao atualizar o paciente: " . $mysqli->error;
-    }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Editar Paciente</title>
+</head>
+<body>
+    <div class="content">
+        <h1>Editar Paciente</h1>
+        <form method="POST">
+            <label for="nome">Nome do Paciente:</label>
+            <input type="text" name="nome" id="nome" value="<?php echo $paciente['nome_paciente']; ?>" required><br>
+
+            <label for="cpf">CPF:</label>
+            <input type="text" name="cpf" id="cpf" value="<?php echo $paciente['cpf_paciente']; ?>" required><br>
+
+            <label for="convenio">Convênio:</label>
+            <select name="convenio" id="convenio" required>
+                <option value="Particular" <?php if ($paciente['convenio_paciente'] == 'Particular') echo 'selected'; ?>>Particular</option>
+                <option value="Público" <?php if ($paciente['convenio_paciente'] == 'Público') echo 'selected'; ?>>Público</option>
+            </select><br>
+
+            <input type="submit" value="Salvar">
+        </form>
+        <br>
+        <a href="lista_pacientes.php">Voltar para a Lista de Pacientes</a> <br>
+        <a href="index.php">Voltar para o login</a>
+    </div>
+    </div>
+</body>
+</html>
--- excluir.php
+++ excluir.php
@@ -1,20 +1,40 @@
 <?php
-include 'conexao.php'; // Inclua o arquivo de conexão ao banco de dados
+include 'conexao.php';
 
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    
-    // Execute a consulta SQL para excluir o paciente com base no ID
-    $sql = "DELETE FROM paciente WHERE id = $id";
-    
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['confirmacao'])) {
+    // Se a confirmação for verdadeira, exclua todos os dados de pacientes
+    $sql = "DELETE FROM paciente";
     if ($mysqli->query($sql)) {
-        echo "Paciente excluído com sucesso.";
+        echo "Todos os dados de pacientes foram excluídos com sucesso!";
     } else {
-        echo "Erro ao excluir o paciente: " . $mysqli->error;
+        echo "Erro ao excluir dados de pacientes: " . $mysqli->error;
     }
-} else {
-    echo "ID do paciente não especificado.";
 }
+
 ?>
 
-<p><a href="index.php">Voltar para a lista de pacientes</a></p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Excluir Todos os Pacientes</title>
+</head>
+<body>
+    <div class="content">
+        <h1>Excluir Todos os Pacientes</h1>
+        <p>Tem certeza de que deseja excluir todos os dados de pacientes? Esta ação não pode ser desfeita.</p>
+       
+        <form method="POST">
+            <label for="confirmacao">Digite "SIM" para confirmar:</label>
+            <input type="text" name="confirmacao" id="confirmacao" required>
+            <input type="submit" value="Excluir Todos os Dados">
+        </form>
+
+        <br>
+        <a href="consultar.php">Voltar para a Lista de Pacientes</a> <br>
+        <a href="index.php">Voltar para o login</a>
+    </div>
+</body>
+</html>
