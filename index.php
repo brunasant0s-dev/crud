a080eaf950295d2c181aa5d272e5144ac5634062
brunasant0s-dev <brunaceciliavitorio@gmail.com>
2023-09-18 13:33:17 -0300

    href

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html>
 <head>
     <title>Cadastro de Paciente</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
     <h1>Cadastro de Paciente</h1>
